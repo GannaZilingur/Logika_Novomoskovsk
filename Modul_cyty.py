65837b8e69c8bd86e8547847b2bbca7b277ceb9d
GannaZilingur <157800805+GannaZilingur@users.noreply.github.com>
2024-01-26 13:37:57 +0200

    Add files via upload

---
+++ Modul_cyty.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+import TurtleVS
+
+speed(150)
+TurtleVS.draw_landscape()
+TurtleVS.draw_sky()
+TurtleVS.draw_block_of_flats()
+TurtleVS.draw_sun()
+TurtleVS.draw_pharmacy()
+hideturtle()
+exitonclick()
+
